af589290b834ba400248055540785b172ffeaeab
Tracy-teb <tracymiriti17@gmail.com>
2024-05-10 14:20:22 +0300

    Create a list of dashes of each letter in the word to be guessed

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import random
 
 hangman_words = ['whizzing', 'fishhook', 'beekeeper', 'rhythm', 'stronghold']
 
-def get_random_word():
+#def get_random_word():
     #Choose a random from the list
-    word = random.choice(hangman_words)
-    return word
+word = random.choice(hangman_words)
+#return word
 #print(get_random_word())
     
-def display_word(word):
+#def display_word(word):
     #Create a list of dashes for each letter in the word to be guessed.
     
-    word_display =['_'] * len(word)
-    return word_display
-#print(' '.join(word_display))  #'single space' joined by the elements of word_display
+word_display =['_'] * len(word)
+    #return word_display
+print(' '.join(word_display))  #'single space' joined by the elements of word_display
 
 
 
