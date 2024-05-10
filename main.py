932802859d016a6ea7d0c26a34ac145c504accc8
Tracy-teb <tracymiriti17@gmail.com>
2024-05-10 14:17:43 +0300

    Define Display Word function

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ def get_random_word():
     #Choose a random from the list
     word = random.choice(hangman_words)
     return word
-print(get_random_word())
+#print(get_random_word())
     
+def display_word(word):
+    #Create a list of dashes for each letter in the word to be guessed.
+    
+    word_display =['_'] * len(word)
+    return word_display
+#print(' '.join(word_display))  #'single space' joined by the elements of word_display
+
 
 
